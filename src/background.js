6356a52b2f91c4facbd8e070541a861ca3d77108
Justyn <Justynboyer@gmail.com>
2019-06-14 21:40:56 +0100

    - Added AutoUpdate - Travis doesn't publish until commits are made to master - Added Comments

--- src/background.js
+++ src/background.js
@@ -2,15 +2,18 @@
 // app starts. It runs through entire life of your application.
 // It doesn't have any windows which you can see on screen, but we can open
 // window from here.
+
 import path from "path";
 import url from "url";
 import { app, Menu } from "electron";
 import { devMenuTemplate } from "./menu/dev_menu_template";
 import { editMenuTemplate } from "./menu/edit_menu_template";
 import createWindow from "./helpers/window";
+import { autoUpdater } from "electron-updater";
+import env from "env";
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
-import env from "env";
+
 const setApplicationMenu = () => {
   const menus = [editMenuTemplate];
   if (env.name !== "production") {
@@ -18,6 +21,8 @@ const setApplicationMenu = () => {
   }
   Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
 };
+autoUpdater.checkForUpdatesAndNotify();
+
 // Save userData in separate folders for each environment.
 // Thanks to this you can use production and development versions of the app
 // on same machine like those are two separate apps.
@@ -25,23 +30,24 @@ if (env.name !== "production") {
   const userDataPath = app.getPath("userData");
   app.setPath("userData", `${userDataPath} (${env.name})`);
 }
+// Make the window
 app.on("ready", () => {
-  setApplicationMenu();
-  const mainWindow = createWindow("main", {
-    width: 1000,
-    height: 600
-  });
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "app.html"),
-      protocol: "file:",
-      slashes: true
-    })
-  );
-  if (env.name === "development") {
-    mainWindow.openDevTools();
-  }
+	setApplicationMenu();
+	const mainWindow = createWindow("main", {
+		width: 1000,
+		height: 600
+	});
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, "app.html"),
+		protocol: "file:",
+		slashes: true
+	}));
+  // Open devtools if in dev stage
+	if (env.name === "development") {
+		mainWindow.openDevTools();
+	}
 });
+// Quit on close
 app.on("window-all-closed", () => {
   app.quit();
 });
