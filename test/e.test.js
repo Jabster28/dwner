69f20c9ac7605c1cc696bc9a49e079a6a6a165f0
Justyn <Justynboyer@gmail.com>
2019-07-02 18:32:00 +0100

    Fixed e2e

---
+++ test/e.test.js
@@ -0,0 +1,28 @@
+const testhelper = require("./helper");
+const app = testhelper.init();
+console.log(testhelper);
+const chaiAsPromised = require("chai-as-promised");
+const chai = require("chai");
+chai.should();
+chai.use(chaiAsPromised);
+
+before( function () {
+  chaiAsPromised.transferPromiseness = app.transferPromiseness;
+  return app.start();
+});
+
+after( function () {
+  if (app && app.isRunning()) {
+    return app.stop();
+  }
+});
+describe("Main", function() {
+  it("open window", function () {
+    return app.client.waitUntilWindowLoaded().getWindowCount().should.eventually.equal(1);
+  });
+  it("JS runs", function () {
+    return app.client.getText("#greet").then(text => {
+      chai.expect(text).to.equal("Hii!!!");
+    });
+  });
+});
