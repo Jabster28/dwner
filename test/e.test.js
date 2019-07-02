949a0e1c9f06a7012fce124a9761d81a36dd9a23
Justyn <Justynboyer@gmail.com>
2019-07-02 18:40:14 +0100

    Debug stuffs

--- test/e.test.js
+++ test/e.test.js
@@ -7,20 +7,25 @@ chai.should();
 chai.use(chaiAsPromised);
 
 before( function () {
+  console.log(0);
   chaiAsPromised.transferPromiseness = app.transferPromiseness;
+  console.log(1);
   return app.start();
 });
 
 after( function () {
+  console.log(4);
   if (app && app.isRunning()) {
     return app.stop();
   }
 });
 describe("Main", function() {
   it("open window", function () {
+    console.log(2);
     return app.client.waitUntilWindowLoaded().getWindowCount().should.eventually.equal(1);
   });
   it("JS runs", function () {
+    console.log(3);
     return app.client.getText("#greet").then(text => {
       chai.expect(text).to.equal("Hii!!!");
     });
